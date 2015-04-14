7dbfc1e97cfeb87712235a8352110794e532a5cc
MaximilianMeister <mmeister@suse.de>
2015-04-14 16:02:28 +0200

    adding a world full of colors
    the colour hue (in the HSV color model) is used to colour bugs
    according to priority, using the standard colours of the heat spectrum
    (e.g. P1=red, P2=orange, P3=yellow, P4=green, P5=blue)
    the colour saturation and/or value (in the HSV color model) is used
    to indicate the age of bugs
    
    So an old P1 would be a faded red, but a new P4 would be bright green.
    
    Closes #11

--- public/js/github.js
+++ public/js/github.js
@@ -1,5 +1,4 @@
-function github(pulltrend, github_url_all_pulls){
-  var colors = ['#B39DDB','#9FA8DA','#90CAF9','#81D4FA','#80DEEA','#80CBC4','#A5D6A7','#C5E1A5','#E6EE9C','#FFF59D','#FFE082','#FFCC80','#FFAB91','#BCAAA4','#EEEEEE'].reverse();
+function github(pulltrend, github_url_all_pulls, colors){
   // GitHub
   new Morris.Line({
     element: 'pull_trend',
@@ -12,7 +11,9 @@ function github(pulltrend, github_url_all_pulls){
     hideHover: true,
     smooth: false,
     continuousLine: true,
-    lineColors: ["#4CAF50"],
+    lineColors: [ colors["line"]["red"],
+                  colors["line"]["green"]
+                ],
     hoverCallback: function (index, options, content, row) {
       return content;
     }
@@ -21,22 +22,5 @@ function github(pulltrend, github_url_all_pulls){
       jQuery.noop();
     else
       window.open(github_url_all_pulls);
-  });
-  $("[creation_time]").each(function (index, value){
-    var one_week = 604800;
-    var two_weeks = 1209600;
-    var one_month = 2629743;
-    var now = Math.round((new Date()).getTime() / 1000);
-    var then = $(this).attr("creation_time");
-    var diff = now - then;
-    if (diff < one_week) {
-      $(value).css("color","#212121").attr('title', 'opened < one week ago');
-    } else if (diff < two_weeks) {
-      $(value).css("color","#E57373").attr('title', 'opened < two weeks ago');
-    } else if (diff < one_month) {
-      $(value).css("color","#C62828").attr('title', 'opened < one month ago');
-    } else {
-      $(value).css("color","#D50000").attr('title', 'opened > one month ago');
-    }
   });
 }
