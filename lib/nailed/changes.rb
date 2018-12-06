0653b9fdb21bc574bd20bd1371e7691f03b76a31
Pascal Arlt <parlt@suse.com>
2018-12-06 12:39:40 +0100

    Split github.rb's code
    Create a new class 'Changes' as parent class of 'Github'.
    Move 'lib/nailed/changes/github.rb's methods into
    lib/nailed/changes.rb, so that they can be used
    for future VCS implementations.
    Octokit specfic functionality remains in github.rb.
    
    Signed-off-by: Pascal Arlt <parlt@suse.com>

---
+++ lib/nailed/changes.rb
@@ -0,0 +1,105 @@
+module Nailed
+
+  def write_allchangetrends
+    Nailed.logger.info("#{__method__}: Writing change trends for all repos")
+    time = Time.new.strftime("%Y-%m-%d %H:%M:%S")
+    origin = Changerequest.select(:origin).distinct.naked.all.map{|o| o[:origin]}
+    origin.each do |vcs|
+      open = Changerequest.where(state: "open", origin: vcs).count
+      closed = Changerequest.where(state: "closed", origin: vcs).count
+      attributes = { time: time,
+                     open: open,
+                     closed: closed,
+                     origin: vcs }
+      begin
+        DB[:allchangetrends].insert(attributes)
+      rescue Exception => e
+        Nailed.logger.error("Could not write all #{origin} change trends:\n#{e}")
+      end
+
+      Nailed.logger.debug("#{__method__}: Saved #{attributes.inspect}")
+    end
+  end
+
+  class Changes
+
+    def get_change_requests(state: 'open')
+      Nailed.logger.info("#{@origin}: #{__method__}")
+      Nailed::Config.all_repositories[@origin].each do |repo|
+        updated_changerequests = []
+        full_repo_name = "#{repo.organization.name}/#{repo.name}"
+        Nailed.logger.info("#{__method__}: Getting #{state} " \
+                           "#{@origin} changerequests " \
+                           "for #{full_repo_name}")
+        begin
+          retries ||= 0
+          changes = change_requests(full_repo_name, state)
+        rescue Exception => e
+          retry if (retries += 1) < 2
+          Nailed.logger.error("Could not get #{@origin} Changes for " \
+                              "#{full_repo_name}: #{e}")
+          next
+        end
+
+        changes.each do |change|
+          attributes = set_attributes(change, {
+                                      rname: repo.name,
+                                      oname: repo.organization.name })
+          begin
+            DB[:changerequests].insert_conflict(:replace).insert(attributes)
+            updated_changerequests.append(attributes[:change_number])
+          rescue Exception => e
+            Nailed.logger.error("Could not write #{@origin} changerequest:\n#{e}")
+            next
+          end
+
+          Nailed.logger.debug(
+            "#{__method__}: Created/Updated #{@origin} changerequest #{repo.name} " \
+            "##{attributes[:change_number]} with #{attributes.inspect}")
+        end unless changes.empty?
+
+        # check for old changerequests of this repo and close them:
+        Changerequest.select(:change_number,
+                             :state,
+                             :oname,
+                             :rname,
+                             :origin).where(state: "open",
+                                           rname: repo.name,
+                                           origin: @origin).each do |change|
+          unless updated_changerequests.include? change.change_number
+            begin
+              change.update(state: "closed")
+              Nailed.logger.info("Closed old #{@origin} changerequest: " \
+                                 "#{repo.name}/#{change.change_number}")
+            rescue Exception => e
+              Nailed.logger.error("Could not close #{@origin} changerequest " \
+                                  "#{repo.name}/#{change.change_number}:\n#{e}")
+            end
+          end
+        end
+
+        write_changetrends(repo.organization.name, repo.name)
+      end
+    end
+
+    def write_changetrends(org, repo)
+      Nailed.logger.info("#{__method__}: Writing #{@origin} change trends for #{org}/#{repo}")
+      open = Changerequest.where(rname: repo, state: "open", origin: @origin).count
+      closed = Changerequest.where(rname: repo, state: "closed", origin: @origin).count
+      attributes = { time: Time.new.strftime("%Y-%m-%d %H:%M:%S"),
+                     open: open,
+                     closed: closed,
+                     oname: org,
+                     rname: repo,
+                     origin: @origin }
+
+      begin
+        DB[:changetrends].insert(attributes)
+      rescue Exception => e
+        Nailed.logger.error("Could not write #{@origin} change trend for #{org}/#{repo}:\n#{e}")
+      end
+
+      Nailed.logger.debug("#{__method__}: Saved #{attributes.inspect}")
+    end
+  end
+end
--- lib/nailed/changes/github.rb
+++ lib/nailed/changes/github.rb
@@ -1,7 +1,7 @@
 require 'octokit'
 
+require_relative '../changes'
 require_relative '../config'
-require_relative '../../nailed'
 require_relative '../../../db/model'
 
 #
@@ -11,17 +11,12 @@ module Nailed
   #
   # github helpers
   #
-  def get_org_repos(github_client, org)
-    all_repos = github_client.org_repos(org)
-    all_repos.map(&:name)
-  end
-
   def list_org_repos(github_client, org)
-    repos = get_org_repos(github_client, org)
+    repos = self.get_org_repos(org)
     repos.each { |r| puts "- #{r}" }
   end
 
-  class Github
+  class Github < Changes
     attr_reader :client
 
     def initialize
@@ -29,100 +24,27 @@ module Nailed
       Octokit.auto_paginate = true
       netrc = Nailed::Config.content["netrc"] || false
       @client = Octokit::Client.new(netrc: netrc)
+      @origin = 'github'
     end
 
-    def get_pull_requests(state: 'open')
-      Nailed.logger.info("Github: #{__method__}")
-      Nailed::Config.all_repositories["github"].each do |repo|
-        updated_pullrequests = []
-        full_repo_name = "#{repo.organization.name}/#{repo.name}"
-        Nailed.logger.info("#{__method__}: Getting #{state} pullrequests " \
-                           "for #{full_repo_name}")
-        begin
-          retries ||= 0
-          pulls = @client.pull_requests("#{full_repo_name}", :state => state)
-        rescue Exception => e
-          retry if (retries += 1) < 2
-          Nailed.logger.error("Could not get Pulls for #{full_repo_name}: #{e}")
-          next
-        end
-        pulls.each do |pr|
-          attributes = { change_number: pr.number,
-                         title: pr.title,
-                         state: pr.state,
-                         url: pr.html_url,
-                         created_at: pr.created_at,
-                         updated_at: pr.updated_at,
-                         closed_at: pr.closed_at,
-                         merged_at: pr.merged_at,
-                         rname: repo.name,
-                         oname: repo.organization.name,
-                         origin: "github" }
-
-          begin
-            DB[:changerequests].insert_conflict(:replace).insert(attributes)
-            updated_pullrequests.append(pr.number)
-          rescue Exception => e
-            Nailed.logger.error("Could not write pullrequest:\n#{e}")
-            next
-          end
-
-          Nailed.logger.debug(
-            "#{__method__}: Created/Updated pullrequest #{repo.name} " \
-            "##{pr.number} with #{attributes.inspect}")
-        end unless pulls.empty?
-
-        # check for old pullrequests of this repo and close them:
-        Changerequest.select(:change_number, :state, :rname).where(state: "open", rname: repo.name).each do |pr|
-          unless updated_pullrequests.include? pr.change_number
-            begin
-              pr.update(state: "closed")
-              Nailed.logger.info("Closed old pullrequest: #{repo.name}/#{pr.change_number}")
-            rescue Exception => e
-              Nailed.logger.error("Could not close pullrequest #{repo.name}/#{pr.change_number}:\n#{e}")
-            end
-          end
-        end
-
-        write_pulltrends(repo.organization.name, repo.name)
-      end
+    def get_org_repos(org)
+      all_repos = self.client.org_repos(org)
     end
 
-    def write_pulltrends(org, repo)
-      Nailed.logger.info("#{__method__}: Writing pull trends for #{org}/#{repo}")
-      open = Changerequest.where(rname: repo, state: "open").count
-      closed = Changerequest.where(rname: repo, state: "closed").count
-      attributes = { time: Time.new.strftime("%Y-%m-%d %H:%M:%S"),
-                     open: open,
-                     closed: closed,
-                     oname: org,
-                     rname: repo,
-                     origin: "github" }
-
-      begin
-        DB[:changetrends].insert(attributes)
-      rescue Exception => e
-        Nailed.logger.error("Could not write pull trend for #{org}/#{repo}:\n#{e}")
-      end
-
-      Nailed.logger.debug("#{__method__}: Saved #{attributes.inspect}")
+    def change_requests(full_repo_name, state)
+      @client.pull_requests(full_repo_name, state: state)
     end
 
-    def write_allpulltrends
-      Nailed.logger.info("#{__method__}: Writing pull trends for all repos")
-      open = Changerequest.where(state: "open").count
-      closed = Changerequest.where(state: "closed").count
-      attributes = { time: Time.new.strftime("%Y-%m-%d %H:%M:%S"),
-                     open: open,
-                     closed: closed,
-                     origin: "github" }
-      begin
-        DB[:allchangetrends].insert(attributes)
-      rescue Exception => e
-        Nailed.logger.error("Could not write allpull trend:\n#{e}")
-      end
-
-      Nailed.logger.debug("#{__method__}: Saved #{attributes.inspect}")
+    def set_attributes(pr, overlay = {})
+      { change_number: pr.number,
+        title: pr.title,
+        state: pr.state,
+        url: pr.html_url,
+        created_at: pr.created_at,
+        updated_at: pr.updated_at,
+        closed_at: pr.closed_at,
+        merged_at: pr.merged_at,
+        origin: @origin }.merge(overlay)
     end
   end
 end
